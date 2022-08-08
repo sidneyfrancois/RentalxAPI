26777bd0b8a2d3b5002a6244db5f9817c4bfe027
Sidney François <sidney.fvm.ps@outlook.com>
2022-08-08 15:02:37 -0300

    inserindo primeira injeção de dependencia

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,4 @@
+import { inject, injectable } from "tsyringe";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -5,15 +6,12 @@ interface IRequest {
   description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
-  /**
-   * O mesmo que:
-   * private categoriesRepository: ICategoriesRepository;
-   * constructor(categoriesRepository: ICategoriesRepository) {
-   *    this.categoriesRepository = categoriesRepository
-   * }
-   */
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
   async execute({ name, description }: IRequest): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import createCategoryController from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { PostgresCategoriesRepository } from "../modules/cars/repositories/implementations/PostgresCategoriesRepository";
@@ -8,10 +8,9 @@ import { PostgresCategoriesRepository } from "../modules/cars/repositories/imple
 const categoriesRoutes = Router();
 const upload = multer({ dest: "./tmp" });
 //const categoriesRepository = new PostgresCategoriesRepository();
+const createCategoryController = new CreateCategoryController();
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
